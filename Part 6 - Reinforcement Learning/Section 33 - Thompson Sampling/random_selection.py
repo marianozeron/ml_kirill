63305fabeb1ecf7ec1ae2ccc9fa4049fe2f6d3eb
Mariano <m.zeron@iruiztechnologies.com>
2019-11-23 16:10:24 +0000

    Not much to say

--- Part 6 - Reinforcement Learning/Section 33 - Thompson Sampling/random_selection.py
+++ Part 6 - Reinforcement Learning/Section 33 - Thompson Sampling/random_selection.py
@@ -1,15 +1,14 @@
 # Random Selection
 
 # Importing the libraries
-import numpy as np
 import matplotlib.pyplot as plt
+import random
 import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
 
 # Implementing Random Selection
-import random
 N = 10000
 d = 10
 ads_selected = []
--- Part 6 - Reinforcement Learning/Section 33 - Thompson Sampling/thompson_sampling.py
+++ Part 6 - Reinforcement Learning/Section 33 - Thompson Sampling/thompson_sampling.py
@@ -1,15 +1,14 @@
 # Thompson Sampling
 
 # Importing the libraries
-import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import random
 
 # Importing the dataset
 dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
 
 # Implementing Thompson Sampling
-import random
 N = 10000
 d = 10
 ads_selected = []
